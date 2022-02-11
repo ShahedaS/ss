a909d58587b0df771f0cbe1e333bdfe809415048
ShahedaS <94096954+ShahedaS@users.noreply.github.com>
2022-02-11 18:44:20 +0530

    Add files via upload

--- springboot/src/main/java/com/dev/springboot/model/Invoice.java
+++ springboot/src/main/java/com/dev/springboot/model/Invoice.java
@@ -4,14 +4,10 @@ package com.dev.springboot.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 	public class Invoice {
 
 	    @Id
